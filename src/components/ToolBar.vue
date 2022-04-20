79b6e3a5f1a53cb1673bccb5dd6980c1222c18a8
joe <joe.chang1014@gmail.com>
2022-04-20 16:38:20 +0800

    feat add fontawesome

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,19 @@
+<script setup>
+import UserCard from '../components/UserCard.vue';
+import { reactive } from 'vue';
+const menuList = reactive([
+  { icon: 'fa-regular fa-thumbs-up', path: '' },
+  { icon: 'fa-solid fa-plus', path: '' },
+  { icon: 'fa-solid fa-house', path: '' },
+  { icon: 'fa-regular fa-bell', path: '' },
+]);
+</script>
+<template>
+  <div class="rounded-[32px]">
+    <ul class="flex justify-between">
+      <li class="border-2 rounded-full flex items-center justify-center">
+        <font-awesome-icon icon="fa-solid fa-user-secret" />
+      </li>
+    </ul>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import App from './App.vue';
 import router from './router';
 import './index.css';
 import { createPinia } from 'pinia';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faUserSecret);
 const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router).use(createPinia()).mount('#app');
