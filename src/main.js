02467fd9033208b53c1a4b135b046b4b2ad44b7f
joe <joe.chang1014@gmail.com>
2022-05-04 22:03:53 +0800

    fix main.js set token

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-localStorage.setItem('access_token', process.env.VUE_APP_IMGUR_ACCESS_TOKEN);
+if (!localStorage.getItem('access_token')) {
+  localStorage.setItem('access_token', process.env.VUE_APP_IMGUR_ACCESS_TOKEN);
+}
 
 library.add(faUserSecret);
 const app = createApp(App);
