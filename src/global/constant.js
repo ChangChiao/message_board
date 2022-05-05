d4129ead435d55e417c72b577c6431f5d40e0565
joe <joe.chang1014@gmail.com>
2022-05-05 20:07:38 +0800

    feat heroku api portal

--- src/global/constant.js
+++ src/global/constant.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:3008/posts';
+// export const API_URL = 'http://localhost:3008/posts';
+export const API_URL = 'https://frozen-island-37694.herokuapp.com/posts';
 export const IMGUR_URL = 'https://api.imgur.com/3/image';
