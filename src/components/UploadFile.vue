a5d55e25f3276cddde60b5158fcfff9b0de3d43b
joe <joe.chang1014@gmail.com>
2022-05-21 13:59:37 +0800

    refactor upload img

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -1,25 +1,25 @@
 <script setup>
-import { useToast } from 'vue-toastification';
+// import { useToast } from 'vue-toastification';
 import { inject } from 'vue';
 import { postFormData } from '../utils/api/ajax.js';
-const toast = useToast();
+// const toast = useToast();
 const controlLoading = inject('controlLoading');
-const getImgurToken = async () => {
-  // eslint-disable-next-line camelcase
-  const { access_token } = await fetch('https://api.imgur.com/oauth2/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: new URLSearchParams({
-      refresh_token: process.env.VUE_APP_IMGUR_REFRESH_TOKEN,
-      client_id: process.env.VUE_APP_IMGUR_CLINETID,
-      client_secret: process.env.VUE_APP_IMGUR_CLINET_SECRET,
-      grant_type: 'refresh_token'
-    })
-  }).then((res) => res.json());
-  localStorage.setItem('access_token', access_token);
-};
+// const getImgurToken = async () => {
+//   // eslint-disable-next-line camelcase
+//   const { access_token } = await fetch('https://api.imgur.com/oauth2/token', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded'
+//     },
+//     body: new URLSearchParams({
+//       refresh_token: process.env.VUE_APP_IMGUR_REFRESH_TOKEN,
+//       client_id: process.env.VUE_APP_IMGUR_CLINETID,
+//       client_secret: process.env.VUE_APP_IMGUR_CLINET_SECRET,
+//       grant_type: 'refresh_token'
+//     })
+//   }).then((res) => res.json());
+//   localStorage.setItem('access_token', access_token);
+// };
 
 const errorList = {
   large: '圖片檔案過大，僅限 1mb 以下檔案',
@@ -45,22 +45,21 @@ const uploadImage = (event) => {
 const sendImgToImgur = async (file) => {
   const formData = new FormData();
   formData.append('image', file);
-  formData.append('type', 'file');
-  // formData.append('album', 'RLtC2t7');
+  // formData.append('type', 'file');
   controlLoading(true);
   try {
-    const result = await postFormData(formData);
-    const { link } = result.data;
-    if (result.status === 200) {
-      emit('setFile', link);
+    const result = await postFormData('/upload', formData);
+    const { imgUrl } = result;
+    if (result.status === 'success') {
+      emit('setFile', imgUrl);
     }
   } catch (error) {
     const { status, data } = error.response;
     console.log(`error--${status}`, 'error', data);
-    toast.error(data?.data?.error);
-    if (status === 403) {
-      getImgurToken();
-    }
+    // toast.error(data?.data?.error);
+    // if (status === 403) {
+    //   getImgurToken();
+    // }
   }
   controlLoading(false);
 };
